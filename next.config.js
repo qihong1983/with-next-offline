0bcd6c2ac37cd7d2337dbf8fb230debbe7c2bacf
qihong <qihong@winsense.ai>
2020-08-24 04:38:55 +0800

    asdf

--- next.config.js
+++ next.config.js
@@ -1,53 +1,53 @@
-
-// module.exports = withOffline({
-//   workboxOpts: {
-//     swDest: process.env.NEXT_EXPORT
-//       ? 'service-worker.js'
-//       : 'static/service-worker.js',
-//     runtimeCaching: [
-//       {
-//         urlPattern: /^https?.*/,
-//         handler: 'NetworkFirst',
-//         options: {
-//           cacheName: 'offlineCache',
-//           expiration: {
-//             maxEntries: 200,
-//           },
-//         },
-//       },
-//     ],
-//   },
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/service-worker.js',
-//         destination: '/_next/static/service-worker.js',
-//       },
-//     ]
-//   },
-// })
-
-
-const withOffline = require('next-offline')
-
+const withOffline = require('next-offline');
 module.exports = withOffline({
   workboxOpts: {
+    swDest: process.env.NEXT_EXPORT
+      ? 'service-worker.js'
+      : 'static/service-worker.js',
     runtimeCaching: [
       {
         urlPattern: /^https?.*/,
-        handler: 'networkFirst',
+        handler: 'NetworkFirst',
         options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
+          cacheName: 'offlineCache',
           expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60
+            maxEntries: 200,
           },
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
-      }
+        },
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/service-worker.js',
+        destination: '/_next/static/service-worker.js',
+      },
     ]
-  }
+  },
 })
+
+
+// const withOffline = require('next-offline')
+
+// module.exports = withOffline({
+//   workboxOpts: {
+//     runtimeCaching: [
+//       {
+//         urlPattern: /^https?.*/,
+//         handler: 'networkFirst',
+//         options: {
+//           cacheName: 'https-calls',
+//           networkTimeoutSeconds: 15,
+//           expiration: {
+//             maxEntries: 150,
+//             maxAgeSeconds: 30 * 24 * 60 * 60
+//           },
+//           cacheableResponse: {
+//             statuses: [0, 200]
+//           }
+//         }
+//       }
+//     ]
+//   }
+// })
